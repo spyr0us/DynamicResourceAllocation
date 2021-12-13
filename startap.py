2759b045cf89a2fc807a876ad9ede62b75801753
spyrous <spyrous@spyrouspc.com>
2021-12-13 23:25:12 +0200

    adding files

---
+++ startap.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import random
+import sys
+import time
+import requests
+from  multiprocessing import dummy
+
+IMAGES_PATH = os.getenv('IMAGES_PATH', './images/')
+IMAGES = []
+for (dirpath, dirnames, filenames) in os.walk(IMAGES_PATH):
+     IMAGES.extend(filenames)
+     break
+
+def post_once():
+     image = os.path.join(IMAGES_PATH, random.choice(IMAGES))
+     print('Using image: {}'.format(image))
+     a = requests.post("http://192.168.49.2:30800", files={"file":
+open(image, "rb")})
+     print(a)
+
+DURATION = float(os.getenv('DURATION', '1600.0'))
+REQUESTS_PER_30 = float(os.getenv('REQUESTS_PER_30', '10.0'))
+POOL = dummy.Pool(16)
+
+def main():
+     start_time = time.time()
+     while (time.time() - start_time) < DURATION:
+         time.sleep(random.expovariate(REQUESTS_PER_30 / 30.0))
+         POOL.apply_async(post_once)
+
+if __name__ == "__main__":
+     main()
+
